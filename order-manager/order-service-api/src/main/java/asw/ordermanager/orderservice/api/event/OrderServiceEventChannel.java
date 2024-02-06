5d6896b8257e185ffa4b167131ea57d7d68f15c0
ValerioPontini <91497060+ValerioPontini@users.noreply.github.com>
2024-02-06 16:21:44 +0100

    Add files via upload

---
+++ order-manager/order-service-api/src/main/java/asw/ordermanager/orderservice/api/event/OrderServiceEventChannel.java
@@ -0,0 +1,5 @@
+package asw.ordermanager.orderservice.api.event;
+
+public class OrderServiceEventChannel {
+	public static final String channel = "new_order";
+}
---
+++ order-manager/order-service-api/src/main/java/asw/ordermanager/orderservice/domain/OrderItem.java
@@ -0,0 +1,14 @@
+package asw.ordermanager.orderservice.domain;
+
+
+
+import lombok.*; 
+
+/* Ordine. */ 
+
+public class OrderItem {
+
+	private String product; 
+	private int quantity; 
+	
+}
